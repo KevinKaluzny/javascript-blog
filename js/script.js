44f5713d9502cb0495665e02a7ae602204d532df
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-03-30 11:48:24 +0200

    Remove line 5 in script.js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@
 
 function titleClickHandler(event) {
     console.log('Link was clicked!');
-    console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,15 +11,15 @@ function titleClickHandler(event) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.post.active');
 
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-
-    /* remove class 'active' from all articles */
 
     /* get 'href' attribute from the clicked link */
 
