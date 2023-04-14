f44aec26ca0c16bb7a6a1cacc6621029fbeedbe0
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-04-14 14:39:18 +0200

    Change the variable allTags in an object

--- js/script.js
+++ js/script.js
@@ -88,8 +88,8 @@ function generateTitleLinks(customSelector = '') {
 generateTitleLinks();
 
 function generateTags(){
-  /* [NEW] create a new variable allTags with an empty array */
-  let allTags = [];
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
 
   /* [DONE] find all articles */
 
