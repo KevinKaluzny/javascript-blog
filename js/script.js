9fb1e58a960fd14999e3f717316e06fc00849297
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-04-19 17:52:04 +0200

    Give html tag <li> around links in generateTags()

--- js/script.js
+++ js/script.js
@@ -182,7 +182,7 @@ function generateTags(){
   /* [NEW] START LOOP: for each tag in allTags: */
   for (let tag in allTags ) {
     /* [NEW] generate code of a link and add it to allTagsHTML */
-    allTagsHTML += '<a href="#tag-' + tag + '" class="' + calculateTagClass(allTags[tag], tagsParams) + '">' + tag + ' (' + allTags[tag] + ') ' + '</a>';
+    allTagsHTML += '<li><a href="#tag-' + tag + '" class="' + calculateTagClass(allTags[tag], tagsParams) + '">' + tag + ' (' + allTags[tag] + ') ' + '</a></li>';
   }
   /* [NEW] END LOOP: for each tag in allTags: */
 
